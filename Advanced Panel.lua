565b344b265dc0a9fba0fa8ea6151da14b723959
JustMiyana <kiyumiazael@gmail.com>
2023-03-12 09:09:44 +0100

    aaaaa

--- Advanced Panel.lua
+++ Advanced Panel.lua
@@ -1,17 +1,3 @@
 return {
     JustMiyana = true,
-	bl_sian = true,
-	Excalidus = true,
-	Mevchh = true,	
-	Nameyeyy = true,
-	slapboxin = true,
-	
-	["FruitySama"] = true,
-	["rJeneration"] = true,
-	["borjal"] = true,
-	["christianstar206"] = true,
-	["simonsoysauce"] = true,
-	["centuriontb"] = true,
-	["LOWARMYDUDE12345"] = true,
-	["VanishingDragone"] = true,
 }
